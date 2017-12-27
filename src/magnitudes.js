5d84c9a07f44a35f01afb86df321364ff6bf3fab
jorbuedo <jorbuedo@me.com>
2017-12-27 20:00:21 +0100

    Magnitudes

---
+++ src/magnitudes.js
@@ -0,0 +1,33 @@
+var m = require("mithril")
+var state = require("./state")
+
+var magnitudes = {
+    view: function(vnode) {
+        var spell = state.combo[m.route.param("tecnica")][m.route.param("forma")]
+        return [
+            m(".rdt", [
+                m("ul", [
+                    m("strong", "Magnitud"),
+                    state.magnitudes.map(function(elem, index){
+                        return m("li", {
+                            onclick:function(){
+                                state.selected.magnitude = index
+                            },
+                            class: state.getSelectionClass(4,state.selected.magnitude, index)
+                        }, elem)
+                    }),
+                ]),
+                m("ul", {style:"width:600px;max-width:100%;"}, [
+                    m("strong", "Descripcion"),
+                    spell.magn.split('#').map(function(elem, index){
+                        return m("li", elem)
+                    }),
+                ]),
+            ])
+        ]
+    }
+}
+
+module.exports = magnitudes
+
+
